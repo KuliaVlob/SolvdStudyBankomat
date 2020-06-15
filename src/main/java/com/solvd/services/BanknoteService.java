37c70f0a533fdedaee92f1bde68988c29dc159e6
unknown <Alsolito9@gmail.com>
2020-06-15 21:49:51 +0300

    clear

--- src/main/java/com/solvd/services/BanknoteService.java
+++ src/main/java/com/solvd/services/BanknoteService.java
@@ -13,129 +13,99 @@ import com.solvd.validator.InfoRefuseValidation;
 public class BanknoteService {
 
 
-  private Usd usd = new Usd();
-  private  Eur eur = new Eur();
-  private UsdDAO usdDAO = new UsdDAO();
-  private EurDAO eurDAO = new EurDAO();
-  private double sumForGettingJSON;
-  private Integer banknoteJSON;
-  private double quantity;
-  private static final Logger LOGGER = LogManager.getLogger(BanknoteService.class);
-  private InfoRefuseValidation infoOfValidation = new InfoRefuseValidation();
+    private Usd usd = new Usd();
+    private Eur eur = new Eur();
+    private UsdDAO usdDAO = new UsdDAO();
+    private EurDAO eurDAO = new EurDAO();
+    private double sumForGettingJSON;
+    private Integer banknoteJSON;
+    private double quantity;
+    private static final Logger LOGGER = LogManager.getLogger(BanknoteService.class);
+    private InfoRefuseValidation infoOfValidation = new InfoRefuseValidation();
 
-  public void getBanknoteUSD(Transaction transaction) {
-    int minBanknote = 10;
-    sumForGettingJSON = transaction.getAmount();
-    banknoteJSON = transaction.getBanknote();
+    public void getBanknoteUSD(Transaction transaction) {
+        int minBanknote = 10;
+        sumForGettingJSON = transaction.getAmount();
+        banknoteJSON = transaction.getBanknote();
 
-    try {
-      usd = usdDAO.getQuantityByBanknoteUSD(banknoteJSON);
-     
-      if (usd.getQuantity().equals("yes")) {// перевіряю чи є в наявності банкнота якщо є то ->
-        convertToBanknote(); // знімаю суму кратну купюрі
-       
-        System.out.println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");// виводжу
-                                                                                                   // на
-                                                                                                   // екран
-                                                                                                   // повідомлення
-                                                                                                   // зняття
-                                                                                                   // першої
-          
-           
-        if ((int) sumForGettingJSON > 0) { // перевіряю чи залишок знятої суми більше 0 якщо так то
-                                           // ->
-          transaction.setBanknote((int) sumForGettingJSON); // присвоюю залишок вхідним данним з
-                                                            // джисона
-          banknoteJSON = transaction.getBanknote();// переоприділяю змінну.. тепер вона дорівнює
-                                                   // залишку
-          for (Usd banknotes : usdDAO.getAvailableBanknoteUSD("yes")) {
-            if (banknotes.getBanknote().equals(banknoteJSON)) { // перевіряю чи залишок дорівнює
-                                                                // банкноті ка є в наявності
-              convertToBanknote();// знімаю залишок з рахунку
-              
-              System.out
-                  .println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");// виводжу
-                                                                                                   // на
-                                                                                                   // екран
-                                                                                                   // повідомлення
-                                                                                                   // про
-                                                                                                   // зняття
-                                                                                                   // залишку
+        try {
+            usd = usdDAO.getQuantityByBanknoteUSD(banknoteJSON);
+            if (usd.getQuantity().equals("yes")) {
+                convertToBanknote();
+                System.out.println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");
+                if ((int) sumForGettingJSON > 0) {
+                    transaction.setBanknote((int) sumForGettingJSON);
+                    banknoteJSON = transaction.getBanknote();
+                    for (Usd banknotes : usdDAO.getAvailableBanknoteUSD("yes")) {
+                        if (banknotes.getBanknote().equals(banknoteJSON)) {
+                            convertToBanknote();
+                            System.out.println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");
+                        }
+                    }
+                    if (sumForGettingJSON > minBanknote) {
+                        convertToBanknote();
+                        System.out.println("You are getting: " + quantity + " By: " + minBanknote + " banknotes");
+                    }
+                }
+            } else {
+                getRefuseInfoUSD();
+                infoOfValidation.getValidationInfo();
             }
-          }
-          if (sumForGettingJSON % minBanknote == 0) { 
-       	   convertToBanknote(); // знімаю суму кратну купюрі
-             
-              System.out.println("You are getting: " + quantity + " By: " + minBanknote + " banknotes");// суми
-          }
-
-         
-        } 
-      } else {
-        getRefuseInfoUSD();
-        infoOfValidation.getValidationInfo();
-      }
-    } catch (PersistenceException e) {
-      LOGGER.error("Sorry, too many connections, please, try again later.");
-      infoOfValidation.getValidationInfo();
-    }
-
-  }
-
-  public void getBanknoteEUR(Transaction transaction) {
-    int minBanknote = 10;
-    sumForGettingJSON = transaction.getAmount();
-    banknoteJSON =transaction.getBanknote();
-
-    try {
-      eur = eurDAO.getQuantityByBanknoteEUR(banknoteJSON);
-
-      if (eur.getQuantity().equals("yes")) {
-        convertToBanknote();
-
-        System.out.println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");
-
-        if ((int) sumForGettingJSON > 0) {
-          transaction.setBanknote((int) sumForGettingJSON);
-          banknoteJSON = transaction.getBanknote();
-
-          for (Eur banknotes : eurDAO.getAvailableBanknoteEUR("yes")) {
-            if (banknotes.getBanknote().equals(banknoteJSON)) {
-              convertToBanknote();
-              System.out.println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");
-            }
-          }
-          if (sumForGettingJSON % minBanknote == 0) {
-            convertToBanknote();
-            System.out.println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");
-          }
+        } catch (PersistenceException e) {
+            LOGGER.error("Sorry, too many connections, please, try again later.");
+            infoOfValidation.getValidationInfo();
         }
-      } else {
-        getRefuseInfoEUR();
-        infoOfValidation.getValidationInfo();
-      }
-    } catch (PersistenceException e) {
-      LOGGER.error("Sorry, too many connections, please, try again later.");
-      infoOfValidation.getValidationInfo();
-    }
-  }
 
-
-  public void getAvailableBanknoteUSD() {
-    try {
-      System.out.println(usdDAO.getAvailableBanknoteUSD("yes"));
-
-    } catch (PersistenceException e) {
-      LOGGER.error("Sorry, too many connections, please, try again later.");
-      infoOfValidation.getValidationInfo();
     }
 
-  }
+    public void getBanknoteEUR(Transaction transaction) {
+        int minBanknote = 10;
+        sumForGettingJSON = transaction.getAmount();
+        banknoteJSON = transaction.getBanknote();
+
+        try {
+            eur = eurDAO.getQuantityByBanknoteEUR(banknoteJSON);
+            if (eur.getQuantity().equals("yes")) {
+                convertToBanknote();
+                System.out.println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");
+                if ((int) sumForGettingJSON > 0) {
+                    transaction.setBanknote((int) sumForGettingJSON);
+                    banknoteJSON = transaction.getBanknote();
+                    for (Eur banknotes : eurDAO.getAvailableBanknoteEUR("yes")) {
+                        if (banknotes.getBanknote().equals(banknoteJSON)) {
+                            convertToBanknote();
+                            System.out.println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");
+                        }
+                    }
+                    if (sumForGettingJSON > minBanknote) {
+                        convertToBanknote();
+                        System.out.println("You are getting: " + quantity + " By: " + banknoteJSON + " banknotes");
+                    }
+                }
+            } else {
+                getRefuseInfoEUR();
+                infoOfValidation.getValidationInfo();
+            }
+        } catch (PersistenceException e) {
+            LOGGER.error("Sorry, too many connections, please, try again later.");
+            infoOfValidation.getValidationInfo();
+        }
+    }
+
+
+    public void getAvailableBanknoteUSD() {
+        try {
+            System.out.println(usdDAO.getAvailableBanknoteUSD("yes"));
+        } catch (PersistenceException e) {
+            LOGGER.error("Sorry, too many connections, please, try again later.");
+            infoOfValidation.getValidationInfo();
+        }
+
+    }
 
     public void getAvailableBanknoteEUR() {
         try {
             System.out.println(eurDAO.getAvailableBanknoteEUR("yes"));
-
         } catch (PersistenceException e) {
             LOGGER.error("Sorry, too many connections, please, try again later.");
             infoOfValidation.getValidationInfo();
@@ -144,23 +114,22 @@ public class BanknoteService {
     }
 
 
-  public void convertToBanknote() {
-    quantity = sumForGettingJSON / banknoteJSON;
-    quantity = (int) quantity;
-    quantity *= banknoteJSON;
-    sumForGettingJSON -= quantity;
+    public void convertToBanknote() {
+        quantity = sumForGettingJSON / banknoteJSON;
+        quantity = (int) quantity;
+        quantity *= banknoteJSON;
+        sumForGettingJSON -= quantity;
+    }
 
-  }
+    public void getRefuseInfoUSD() {
+        System.out.println("Unfortunately, the ATM does not have the banknotes you need");
+        System.out.println("The following banknotes are available at the ATM");
+        getAvailableBanknoteUSD();
+    }
 
-  public void getRefuseInfoUSD() {
-    System.out.println("Unfortunately, the ATM does not have the banknotes you need");
-    System.out.println("The following banknotes are available at the ATM");
-    getAvailableBanknoteUSD();
-  }
-
-  public void getRefuseInfoEUR() {
-      System.out.println("Unfortunately, the ATM does not have the banknotes you need");
-      System.out.println("The following banknotes are available at the ATM");
-      getAvailableBanknoteEUR();
+    public void getRefuseInfoEUR() {
+        System.out.println("Unfortunately, the ATM does not have the banknotes you need");
+        System.out.println("The following banknotes are available at the ATM");
+        getAvailableBanknoteEUR();
     }
 }
